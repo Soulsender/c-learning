816ae279729dc6d018d2bea54ffa5949e8dcfe82
Soulsender <soulsender@soulsender.me>
2023-03-14 23:21:38 -0700

    commit dataTypes

---
+++ dataTypes/dataTypes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    // int is a whole number
+    int integer = 5;
+
+    // double is a decimal number
+    double decimalDouble = 4.68;
+    double decimalDouble2 = 4.0;
+
+    // char are characters
+    char singleChar = "a";
+    char thisIsAString[] = "Hello World";
+
+    return 0;
+}
