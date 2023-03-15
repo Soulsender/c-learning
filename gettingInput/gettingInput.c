cafbc0a0f9bab0a43b8598af108c57866170f403
Soulsender <soulsender@soulsender.me>
2023-03-14 23:40:17 -0700

    userInput

---
+++ gettingInput/gettingInput.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int age;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("You are %d years old.\n", age);
+
+    return 0;
+}
