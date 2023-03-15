9ab42574ad19ddee13a3f12cf7b9551c8b8c41b3
Soulsender <soulsender@soulsender.me>
2023-03-14 23:15:23 -0700

    init

---
+++ variables/variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    // strings are arrays of chars
+    char name[] = "John";
+    int age = 38;
+
+    // %s means a string
+    printf("There was a man named %s.\n", name);
+
+    // %d means an int
+    printf("The man was %d years old.", age);
+
+    return 0;
+}
